9fc39a6a7be35a16f370023163aa9cf694771567
zhangletian.zj <zhangletian.zj@bytedance.com>
2025-06-04 17:20:27 +0800

    fix qwen2.5-vl oasis

--- infer_pipeline.py
+++ infer_pipeline.py
@@ -12,27 +12,55 @@ import argparse
 import yaml
 import contextlib
 import gc
-from swift.llm import InferRequest, RequestConfig, VllmEngine
-from utils import load_data
+import vllm
+from vllm.distributed.parallel_state import (destroy_distributed_environment, destroy_model_parallel)
+# from swift.llm import InferRequest, RequestConfig, VllmEngine
+from utils import load_data, bad_word_processor
 import time
 try:
     torch.multiprocessing.set_start_method('spawn')
 except:
     pass
 os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
+import math
 
-from oasis.hack_VllmEngine import VllmEngine
-sys.modules["swift.llm"].VllmEngine = VllmEngine
+# from oasis.new_class import VllmEngine
+# sys.modules["swift.llm"].VllmEngine = VllmEngine
+from vllm import LLM, SamplingParams
+from qwen_vl_utils import process_vision_info
+from transformers import AutoProcessor
+
+from pathlib import Path
+from tqdm import tqdm
+
+import psutil
 
 class MMINFDataSynthesisPipeline:
     def __init__(self):
         self.engine = None
         self.model_id_or_path = None
+        self.doubao_vision = False
 
     def destroy_model(self):
-        pass
+        print('This function should not be called.')
+        return
+        print('[INFO] destroy model')
+        if self.engine:
+            del self.engine.llm_engine.model_executor
+            del self.engine.llm_engine
+            del self.engine
+            # self.engine.llm_engine.stop_remote_worker_execution_loop()
+            destroy_model_parallel()
+            destroy_distributed_environment()
+            # del self.engine.llm_engine.engine_core.engine_core.model_executor.driver_worker.model_runner.model
+            with contextlib.suppress(AssertionError):
+                torch.distributed.destroy_process_group()
+            gc.collect()
+            torch.cuda.empty_cache()
+            ray.shutdown()
 
-    def update_model(self, model_id_or_path, max_model_len=32768, limit_mm_per_prompt=None, batch_size=128):
+
+    def update_model(self, model_id_or_path, model_args):
         """
         更新模型
         :param model_id_or_path: 模型的路径或ID
@@ -47,26 +75,27 @@ class MMINFDataSynthesisPipeline:
             self.destroy_model()
         
         world_size = torch.cuda.device_count()
-        self.engine = VllmEngine(
+        model_args.setdefault("max_model_len", 32768)
+        model_args.setdefault("limit_mm_per_prompt", None)
+        model_args.setdefault("tensor_parallel_size", world_size)
+        model_args.setdefault("max_num_seqs", 128)
+        model_args.setdefault("gpu_memory_utilization", 0.8)
+        self.engine = LLM(
             model_id_or_path,
-            max_model_len=max_model_len,
-            limit_mm_per_prompt=limit_mm_per_prompt,
-            tensor_parallel_size=world_size,
-            max_num_seqs=batch_size,
-            gpu_memory_utilization=0.8,
+            distributed_executor_backend='mp',
+            disable_custom_all_reduce=True,
+            **model_args
         )
+        self.processor = AutoProcessor.from_pretrained(model_id_or_path)
 
     def format_prompt(self, prompt_raw, query_items):
         try:
-            # 如果prompt定义在python文件中，则使用importlib导入
             module_name, func_name = prompt_raw.rsplit(".", 1)
             module = importlib.import_module(module_name)
             prompt_func = getattr(module, func_name)
             prompt = prompt_func(*query_items)
         except:
-            # 如果prompt是字符串，则直接使用format方法，并检查{}占位符的数量是否和query_items一致
-            # 预期prompt里的{}占位符数量和query_items相等，才可以正常生成prompt
-            assert prompt_raw.replace("{{", "").replace("}}", "").count("{}") == len(query_items), f"Number of placeholders ({prompt_raw.count('{}')}) does not match the number of query items ({len(query_items)})"
+            assert prompt_raw.replace("{{", "").replace("}}", "").count("{}") == len(query_items)
             prompt = prompt_raw.format(*query_items)
         
         return prompt
@@ -84,49 +113,45 @@ class MMINFDataSynthesisPipeline:
         videos = data.get('videos', [])
         prompt = self.format_prompt(prompt_raw, query_items)
 
-        request_message = {
+        messages = []
+        if sys_prompt:
+            messages.append({'role': 'system', 'content': sys_prompt})
+
+        user_message = {
             'role': 'user',
             'content': []
         }
 
-
         if '<image>' in prompt:
-            num_images = prompt.count('<image>')
-            if num_images != len(images):
-                raise ValueError(f'Number of images ({len(images)}) does not match the number of <image> placeholders ({num_images}) in the prompt.')
-            request_message['content'] = prompt
-            return InferRequest(messages=[request_message], images = images)
-        else:
-            for image in images:
-                request_message['content'].append(
-                    {
-                        'type': 'image',
-                        'image': image
-                    }
-                )
-            for audio in audios:
-                request_message['content'].append(
-                    {
-                        'type': 'audio',
-                        'audio': audio
-                    }
-                )
-            for video in videos:
-                request_message['content'].append(
-                    {
-                        'type': 'video',
-                        'video': video
-                    }
-                )
-            request_message['content'].append(
+            prompt = prompt.replace('<image>','')
+            
+        for image in images:
+            user_message['content'].append(
                 {
-                    'type': 'text',
-                    'text': prompt
+                    'type': 'image',
+                    'image': image
                 }
             )
-            return InferRequest(messages=[request_message])
+        for video in videos:
+            user_message['content'].append(
+                {
+                    'type': 'video',
+                    'video': video
+                }
+            )
+        user_message['content'].append(
+            {
+                'type': 'text',
+                'text': prompt
+            }
+        )
+        if len(user_message['content']) == 1 and user_message['content'][0]['type'] == 'text':
+            user_message['content'] = user_message['content'][0]['text']
+        messages.append(user_message)
+            
+        return messages
 
-    def infer(self, step_name, model_id_or_path, input_file, output_file, save_dir, prompt_func, sys_prompt=None, enable_history=True, oasis=False, batch_size=128):
+    def infer(self, step_name, model_id_or_path, model_args, input_file, output_file, save_dir, prompt_func, request_args={}, sys_prompt=None, enable_history=True, oasis=False, **kwargs):
         """
         进行推理
         :param step_name: 步骤名称
@@ -140,10 +165,10 @@ class MMINFDataSynthesisPipeline:
         """
         input_data = load_data(input_file)
 
-        # 检查是否有推理结果的缓存，如果有，则从缓存中读取，只推理没有推理结果的数据
         cache_dir = os.path.join(save_dir, 'cache')
         cache_file = os.path.join(cache_dir, 'cache.jsonl')
         os.makedirs(cache_dir, exist_ok=True)
+
         if not os.path.exists(cache_file) or len(load_data(cache_file)) == 0:
             with open(cache_file, 'w') as f:
                 json.dump({}, f)
@@ -154,28 +179,44 @@ class MMINFDataSynthesisPipeline:
         input_data = input_data[processed_num:]
         if not input_data:
             return
+
+        batch_size = model_args.get('max_num_seqs', 128)
+        self.update_model(model_id_or_path, model_args)
         
-        # 检查是否需要切换模型
-        self.update_model(model_id_or_path, batch_size=batch_size)
-        
-        # 创建推理请求
-        infer_requests = []
-        request_config = RequestConfig(max_tokens=1024, temperature=0)
+        request_args.setdefault('max_tokens', 1024)
+        request_args.setdefault('temperature', 0)
+        sampling_params = SamplingParams(**request_args)
+        sampling_params.encode_hack = False
+
         if oasis:
-            request_config.encode_hack = True
-            request_config.hack_idx = -5
-            request_config.temperature = 1.0
-            request_config.top_k = 20
-            request_config.top_p = 1.0
-            request_config.min_tokens = 2
-            request_config.repetition_penalty = 1.0
-            request_config.max_tokens = 512
-            request_config.top_logprobs = 10
+            sampling_params.encode_hack = True
+            sampling_params.hack_idx = -5
+            sampling_params.temperature = 1.0
+            sampling_params.top_k = 20
+            sampling_params.top_p = 1.0
+            sampling_params.repetition_penalty = 1.0
+            sampling_params.max_tokens = 512
+            sampling_params.min_tokens = 50
+            
+            if sampling_params.logits_processors is None:
+                sampling_params.logits_processors = []
+                try:
+                    from transformers import AutoConfig
+                    config = AutoConfig.from_pretrained(model_id_or_path)
+                    model_type = config.model_type
+
+                    if 'qwen2_5_vl' in model_type:
+                        sampling_params.frequency_penalty = 1.0
+                        sampling_params.presence_penalty = 1.0
+                        sampling_params.logits_processors = [bad_word_processor]
+                except:
+                    pass
 
         if not os.path.exists(output_file) or processed_num == 0:
             with open(output_file, 'w') as f:
                 pass
-        
+
+        infer_requests = []
         for data in input_data:
             infer_requests.append(self.prepare_data(data, prompt_func, sys_prompt=sys_prompt))
 
@@ -204,16 +245,54 @@ class MMINFDataSynthesisPipeline:
             return data
 
         with open(output_file, 'a') as f:
-            for i in range(0, len(infer_requests), batch_size):
+            total_batches = math.ceil(len(infer_requests) / batch_size)
+
+            outer_pbar = tqdm(
+                range(0, len(infer_requests), batch_size),
+                total=total_batches,
+                desc="Total Inference",
+                position=0,
+                leave=False
+            )
+
+            for i in outer_pbar:
                 # 获取当前批次的推理请求
-                batch_infer_requests = infer_requests[i:i + batch_size]
-                resp_list = []
-                resp_list = self.engine.infer(batch_infer_requests, request_config)
-                resp_list = [resp.choices[0].message.content for resp in resp_list]
-                for data, resp in zip(input_data[i:i + batch_size], resp_list):
-                    # 保存推理结果到history中
-                    data = post_process(step_name, prompt_func, data, resp, enable_history)
-                    f.write(json.dumps(data) + '\n')
+                batch_infer_requests = infer_requests[i:i + batch_size] # list of messages
+                try:
+                    inputs = []
+                    for messages in batch_infer_requests:
+                        message = {}
+                        message["prompt"] = self.processor.apply_chat_template(
+                            messages, tokenize=False, add_generation_prompt=True
+                        )
+                        # Oasis truncate prompt
+                        if sampling_params.encode_hack:
+                            last_index = message["prompt"].rfind("<|im_end|>")
+                            if last_index != -1:
+                                message["prompt"] = message["prompt"][:last_index]
+
+                        if type(messages[0]['content']) == list:
+                            image_inputs, video_inputs = process_vision_info(messages, return_video_kwargs=False)
+                            message["multi_modal_data"] = {}
+                            if image_inputs:
+                                message["multi_modal_data"]["image"] = image_inputs
+                            if video_inputs:
+                                message["multi_modal_data"]["video"] = video_inputs
+                        inputs.append(message)
+
+                    resp_list = self.engine.generate(inputs, sampling_params=sampling_params, use_tqdm=False)
+                    
+                    resp_list = [resp.outputs for resp in resp_list]
+                    for data, choice_list in zip(input_data[i:i + batch_size], resp_list):
+                        resp = [choice.text for choice in choice_list]
+                        if len(resp) == 1:
+                            resp = resp[0]
+                        # 保存推理结果到history中
+                        data = post_process(step_name, prompt_func, data, resp, enable_history)
+                        f.write(json.dumps(data) + '\n')
+                except Exception as e:
+                    tqdm.write(f"[ERROR] Infer failed on batch {i//batch_size}: {e}. Please check your input data (especially images)")
+                    pass
                 cache_data[step_name] += len(batch_infer_requests)
                 with open(cache_file, 'w') as f_cache:
                     json.dump(cache_data, f_cache)
@@ -239,6 +318,8 @@ def exec_step(config, mminf_pipeline, input_file, save_dir, enable_history, step
     
     # 获取这个step的模型checkpoint路径
     model_id_or_path = config.get('model_id_or_path')
+    model_args = config.get('model_args', {})
+    request_args = config.get('request_args', {})
     # 获取这个step的后处理函数
     pp_func_list = config.get('pp_func', [])
     if pp_func_list is None:
@@ -249,7 +330,7 @@ def exec_step(config, mminf_pipeline, input_file, save_dir, enable_history, step
     if not os.path.exists(input_file):
         input_file = os.path.join(save_dir, input_file)
 
-    if model_id_or_path: # 如果有模型，就做推理，否则跳过推理做后处理
+    if model_id_or_path:
         prompt = config['prompt']
         sys_prompt = config.get('sys_prompt', None)
         output_file = os.path.join(save_dir, step_name + '_mid.jsonl')
@@ -260,9 +341,11 @@ def exec_step(config, mminf_pipeline, input_file, save_dir, enable_history, step
         mminf_pipeline.infer(
             step_name,
             model_id_or_path,
+            model_args,
             input_file,
             output_file,
             save_dir,
+            request_args = request_args,
             sys_prompt = sys_prompt,
             prompt_func=prompt,
             enable_history = enable_history,
